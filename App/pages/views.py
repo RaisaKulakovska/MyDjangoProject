449ebea52b03290d44ce39b4ee7b37c90fae8d6d
unknown <Raisakulakovska@gmail.com>
2020-03-22 22:28:26 +0300

    was

---
+++ App/pages/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):    
+    return render(request, 'pages/index.html')
+
+def about(request):
+    data = {'title': "About Us"}
+    return render(request, 'pages/about.html', data)
+
+def services(request):
+    data = {'title': "Our Services"}
+    return render(request, 'pages/services.html', data)
+
+def contact(request):
+    data = {'title': "Contact Us"}
+    return render(request, 'pages/contact.html', data)
