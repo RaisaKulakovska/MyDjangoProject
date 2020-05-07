98476d01849c6bcc5c162172f8e495603ad2a16e
unknown <Raisakulakovska@gmail.com>
2020-05-07 19:58:58 +0300

    fin5

--- App/App/settings.py
+++ App/App/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "app/static")
+    os.path.join(BASE_DIR, "App/static")
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
